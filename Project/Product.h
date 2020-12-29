88c42c601e1a58f2e6333232be8c05dced827a23
Yuriy Petkun <antiko26@bk.ru>
2020-12-29 16:59:04 +0300

    initialization Product.h

---
+++ Project/Product.h
@@ -0,0 +1,15 @@
+#ifndef __PRODUCT_H__
+#define __PRODUCT_H__
+#include "Struct.h"
+using namespace std;
+
+class Product  // Товар
+{
+private:
+
+public:
+	Product(); // Конструктор
+	~Product(); // Деструктор
+
+
+#endif
