f0adbea92a55ff577d9953175d09b6a9bf5695aa
kreker_231 <kohanovichartem@gmail.com>
2020-12-29 17:16:48 +0300

    inicialization Expenses.h

---
+++ Project/Expenses.h
@@ -0,0 +1,17 @@
+#ifndef __EXPENSES_H__
+#define __EXPENSES_H__
+#include "Struct.h"
+#include "Product.h"
+using namespace std;
+
+class Expenses // Расход
+{
+private:
+	
+public:
+	Expenses();
+	~Expenses();
+	
+};
+
+#endif
