cd0bbe5bdb340f2c2fc774a2805211d9544f8c9b
Daniil <dynkachannel@gmail.com>
2020-12-29 17:18:32 +0300

    Initialization Income.h

---
+++ Project/Income.h
@@ -0,0 +1,16 @@
+#ifndef __INCOME_H__
+#define __INCOME_H__
+#include "Struct.h"
+#include "Product.h"
+using namespace std;
+
+class Income // Доход
+{
+private:
+
+public:
+	Income();
+	~Income();
+};
+
+#endif
